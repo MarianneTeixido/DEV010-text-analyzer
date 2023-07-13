69b66475bfb513a595f0f0b41343f4eb9e552a77
Marianne Teixido <ztp.txdo@gmail.com>
2023-07-13 12:47:04 -0600

    add reset button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+const text = document.getElementById('mensaje');
+text.value = '';
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', function handleClick() {
+//  console.log(textarea.value);
+  text.value = '';
+});
+
+
