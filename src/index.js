7ad82160c59130f303c5c140b2a861f7f778eacf
Marianne Teixido <ztp.txdo@gmail.com>
2023-07-17 12:22:52 -0600

    reset button

--- src/index.js
+++ src/index.js
@@ -16,10 +16,14 @@ document.getElementById("msn6").textContent = "Longitud media de palabras: 0" ;
 
 
 btn.addEventListener('click', function handleClick() {
-// console.log(text.value);
-//  analyzer.getWordCount(text); // invocacion
-
   text.value = '';
+  document.querySelector('[data-testing="word-count"]').textContent = "Recuento de palabras: 0";
+  document.querySelector('[data-testing="character-count"]').textContent = "Recuento de caracteres: 0";
+  document.querySelector('[data-testing="character-no-spaces-count"]').textContent = "Recuento de caracteres sin espacios: 0";
+  document.querySelector('[data-testing="number-count"]').textContent = "Recuento de números: 0";
+  document.querySelector('[data-testing="number-sum"]').textContent = "Suma total de números: 0";
+  document.querySelector('[data-testing="word-length-average"]').textContent = "Longitud media de palabras: 0";
+
 });
 
 //Imprime en el DOM el recuento de palabras
