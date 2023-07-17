8798446837e14d1b8a67caefa19dd9e256d68023
Marianne Teixido <ztp.txdo@gmail.com>
2023-07-17 11:18:09 -0600

    added js functionalities

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,21 +1,55 @@
 
 
 const analyzer = {  
+  
   getWordCount: (text) => {
-    
-    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+//TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const words = text.value.trim().split(" ");
+
+    //function totalWords (){
+    return words.length; 
+    //}
+    //console.log("Palabras totales " + words.length);
   },
+    
+
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    const charC =  text.value;
+    return charC.length;
+    console.log("Caracteres totales" +  charC.length)
   },
+
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    const charCS = text.value.split(" ");
+    let str = 0;
+    for(let i= 0; i<charCS.length; i++){
+      str = str + charCS[i].length;
+    }
+    return str;
+    //console.log(str)
+    //return charCS.length;
   },
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (text) => {   
+    const wordL = text.value.trim().split(" ");
+    let str2 = 0;
+    for (let i = 0; i < wordL.length; i++){
+      str2 = str2 + wordL[i].length
+    } 
+    return str2/wordL.length;
+    //console.log(str2/wordL.length)
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberCount: (text) => {
+    const regex = /\d+/g;
+    let numC = text.value.match(regex);
+    if(numC != null ){
+      return numC.length;
+      //console.log(numC.length)
+    }
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+  
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
--- src/index.js
+++ src/index.js
@@ -3,12 +3,45 @@ import analyzer from './analyzer.js';
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 
 const text = document.getElementById('mensaje');
-text.value = '';
+//text.value = '';
 const btn = document.getElementById('btn');
+let numeroCaracteres = text.length;
+
+document.getElementById("msn1").textContent = "Recuento de palabras: 0" ;
+document.getElementById("msn2").textContent = "Recuento de caracteres: 0" ;
+document.getElementById("msn3").textContent = "Recuento de caracteres sin espacios: 0" ;
+document.getElementById("msn4").textContent = "Recuento de números: 0" ;
+document.getElementById("msn6").textContent = "Longitud media de palabras: 0" ;
+
 
 btn.addEventListener('click', function handleClick() {
-//  console.log(textarea.value);
+// console.log(text.value);
+//  analyzer.getWordCount(text); // invocacion
+
   text.value = '';
 });
 
+//Imprime en el DOM el recuento de palabras
+text.addEventListener("keyup", (event) => {
+  let wordCount = analyzer.getWordCount(text); // invocacion
+  let chatCount = analyzer.getCharacterCount(text);
+  let charCountWoutSpace = analyzer.getCharacterCountExcludingSpaces(text);
+  let wordLength = analyzer.getAverageWordLength(text);
+  let numCount = analyzer.getNumberCount(text);
+  //console.log(wordCount);
+  document.getElementById("msn1").textContent = "Recuento de palabras: "+ wordCount.toString();
+  document.getElementById("msn2").textContent = "Recuento de caracteres: "+ chatCount.toString();
+  document.getElementById("msn3").textContent = "Recuento de caracteres sin espacios: "+ charCountWoutSpace.toString();
+  document.getElementById("msn6").textContent = "Longitud media de palabras: "+ wordLength.toString();
 
+  if(numCount != null){
+    document.getElementById("msn4").textContent = "Recuento de números: "+ numCount.toString();
+
+  }
+});
+
+
+//llamar y manipular
+//getElement Querry
+//Innerr Text
+//text 
